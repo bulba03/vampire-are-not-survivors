c26c588c286ba6a872bf1fd150adde22f41a31b2
Mikhail Bulba <bulbas.beats@gmail.com>
2024-04-15 00:57:52 +0300

    up: change project name in .toml

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use bevy::diagnostic::LogDiagnosticsPlugin;
 pub mod player;
 pub mod animation;
 fn main() {
-    App::new().add_plugins(DefaultPlugins)
-    .add_plugins(LogDiagnosticsPlugin::default())
-    .add_plugins((animation::AnimationPlugin, player::PlayerPlugin))
-    .add_systems(Startup, setup_cam)
-    .run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins((animation::AnimationPlugin, player::PlayerPlugin))
+        .add_systems(Startup, setup_cam)
+        .run();
 }
 
 fn setup_cam(mut commands: Commands) {
